2bd1c8a4c5d91f262b40959bfad8a30b754315cb
ibrahimdirik <hidirik@gmail.com>
2023-06-10 07:36:56 +0300

    Food delivery add user feature added.

---
+++ src/main/java/food/delivery/entities/User.java
@@ -0,0 +1,61 @@
+package food.delivery.entities;
+
+
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "userTable")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "email")
+    private String email;
+
+    public User() {
+    }
+
+    public User(String name, String lastName, String emailId) {
+        super();
+        this.name = name;
+        this.lastName = lastName;
+        this.email = emailId;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
